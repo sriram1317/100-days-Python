8671bb110a334894ae5fd81b354190b2a773b7aa
Sriram N V <sriram.vilvanathan@gmail.com>
2023-06-05 11:58:54 +0530

    Basic concepts of input funcion

---
+++ inputFunction.py
@@ -0,0 +1,10 @@
+# Input funciton is used to get an input from the user, it has an optional argument for printing a prompt
+
+# Snippet 1
+# simple input, but doing nothing with the input data
+# input("What's your name?")
+
+
+# Snippet 2
+# now its doing something with the input
+print("Hello " + input("What's your name? ") + '!')
