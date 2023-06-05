a45c61825659a09478dd79e13bf0d9f920f037ba
Sriram N V <sriram.vilvanathan@gmail.com>
2023-06-05 12:12:20 +0530

    Exercise 2

---
+++ exercise2.py
@@ -0,0 +1,12 @@
+# Exercise: Write a program that sitches values stored in variables a and b
+
+# Snippet 1
+a = input("Provide value for a: ")
+b = input("Provide value for b: ")
+
+c = a
+a = b
+b = c
+
+print("Value of a is " + a)
+print("Value of b is " + b)
