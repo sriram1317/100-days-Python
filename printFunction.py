886456cc63b43c01239f410a486b779a881d4856
Sriram N V <sriram.vilvanathan@gmail.com>
2023-06-05 11:50:53 +0530

    Basic concepts of print function and string concatanation

---
+++ printFunction.py
@@ -0,0 +1,5 @@
+# print('Day 1 - Python Print funtion')
+# print('The function is declared like this')
+# print('print("what to print")')
+
+print('Day 1 - Python Print funtion\nThe function is declared like this\nprint("what to print")')
---
+++ stringConcatanation.py
@@ -0,0 +1,17 @@
+# basic string concatanation concept
+
+# Snippet 1
+print("Hello")
+print("lorem ipsum")
+# output 1
+# Hello
+# lorem ipsum
+
+# Snippet 2
+print('Hello' + 'Lorem ipsum')
+# output 2
+# HelloLorem ipsum
+
+# Snippet 3
+print('Hello' + ' Lorem ipsum')
+# Hello Lorem ipsum
