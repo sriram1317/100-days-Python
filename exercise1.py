144a86d0831558042d74fe2691021f321718210c
Sriram N V <sriram.vilvanathan@gmail.com>
2023-06-05 12:05:33 +0530

    First exercise

---
+++ exercise1.py
@@ -0,0 +1,9 @@
+# write a program that prints the number of charecters in a user's name (can use a function if needed).
+
+# Snippet 1
+# name = input("Hello! Whats your name? ")
+# print(len(name))
+
+
+# Snippet 2
+print(len(input("Hi! What's your Name? ")))
