da5a371394596082dd8b7b272cf6a566f5f0a937
Sriram N V <sriram.vilvanathan@gmail.com>
2023-06-06 11:57:38 +0530

    Exercise 1 updated

--- Day_2/exercise1.py
+++ Day_2/exercise1.py
@@ -1,11 +1,16 @@
 # write a program that adds two digit numbers. e.g if the input is 35 then the output should be 3 + 5 = 8
 
-num = int(input("Enter a two digit number: "))
+num = input("Enter a two digit number: ")
 sum = 0
 
-while num > 0:
-    r = num % 10
-    sum += r
-    num = int(num / 10)
-
+sum = int(num[0]) + int(num[1])
 print(sum)
+
+
+# while num > 0:
+#     r = num % 10
+#     sum += r
+#     num = int(num / 10)
+
+# print(sum)
+
