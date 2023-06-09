35afe274d422165b8d0675c7a70085a82969bb94
Sriram N V <sriram.vilvanathan@gmail.com>
2023-06-09 19:04:46 +0530

    exercise 2

---
+++ Day_3/exercise2.py
@@ -0,0 +1,8 @@
+# Find a given number is even or odd
+
+num = int(input("Enter a number to find if its even or odd: "))
+
+if num % 2 == 0:
+    print(f'{num} is even')
+else:
+    print(f'{num} is odd')
