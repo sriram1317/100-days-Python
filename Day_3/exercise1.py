ed21e2c89972b03e85f521a0ee7206de7a06ab60
Sriram N V <sriram.vilvanathan@gmail.com>
2023-06-09 18:58:17 +0530

    exercise 1

--- Day_3/exercise1.py
+++ Day_3/exercise1.py
@@ -3,7 +3,7 @@
 print("Welcome to te rollercoaster")
 height = int(input("Enter your Height (in CMs): "))
 
-if height > 120:
+if height >= 120:
     print("Yay, you are good to ride")
 else:
     print("!Sorry!, you are not tall enough to ride this ride")
