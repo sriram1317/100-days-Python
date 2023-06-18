2219562834ceffd109d43fc89f6732f3174507a7
Sriram N V <sriram.vilvanathan@gmail.com>
2023-06-18 13:19:52 +0530

    Working program finished

--- Day_7/hangMan.py
+++ Day_7/hangMan.py
@@ -14,33 +14,64 @@
 # 1: Generate or pick randon word.
 
 import random
+import os
 
 words = ["pleasant", "shark", "tolerate", "husband", "effort","spare","skeleton","top","humanity","ideal"]
 usedWords = []
+endOfGame = False
 
 def getWord(words, usedWords):
     word = random.choice(words)
-    usedWords.append(word)    
     if word not in usedWords:
+        usedWords.append(word)  
         return word
     else:
         getWord(words,usedWords)        
 
-life = 3
-remLetters = 0
+def replaceSpace(spaces,guess,word):
+    for i in range(0,len(word)):
+        if guess == word[i]:
+            spaces[i] = guess
+    return spaces
+
 
 word = getWord(words, usedWords)
-spaces = ""
+remLetters = len(word) 
+life = 6
+
+spaces = []
 for i in word:
     spaces += '_'
 
-
-guess = input("Guess the letter: ")
-if guess in word:
-    print("Correct")
-else:
-    print("Wrong")
-    life -= 1
-    
 print(f"Word={word}")
-print(f"Spaces={spaces}")
+
+
+while endOfGame == False:   
+    
+    # print(f"Spaces={spaces}")
+    guess = input("Guess the letter: ")
+    os.system('cls')
+    if guess in word:
+        
+        print(f"Correct!, letter {guess} is present")
+        spaces = replaceSpace(spaces, guess, word)
+        print(f"Spaces={spaces}")
+        c = word.count(guess)
+        remLetters -= c
+    
+    else:
+        print("Wrong")
+        print(spaces)
+
+        life -= 1
+    
+    if remLetters == 0 or life == 0:
+        endOfGame = True
+
+    
+    
+
+if life > 0:
+    print("!Yay! you won")
+else:
+    print("OHH NO, you lost")
